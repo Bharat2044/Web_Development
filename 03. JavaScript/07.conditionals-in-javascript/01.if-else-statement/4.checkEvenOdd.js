209e1eff1ebd10cfaf6a5ab8eda0be8cbe7c1903
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 19:14:18 +0000

    code added

---
+++ 03. JavaScript/07.conditionals-in-javascript/01.if-else-statement/4.checkEvenOdd.js
@@ -0,0 +1,10 @@
+let num = 6;
+if(num == 0) {
+    console.log(num + " is neither an odd number nor an even number");
+}
+else if(num%2 == 0) {
+    console.log(num + " is an even number");
+}
+else {
+    console.log(num + " is an odd number");
+}
