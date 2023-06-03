b9d1c5a050111e85449ff387fed0180c3cf87846
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 18:53:17 +0000

    code added

---
+++ 03. JavaScript/04.datatype-typeconversion-in-javascript/03.otherTypeToStringConversion.js
@@ -0,0 +1,59 @@
+let a = 123;
+let b = String(a);
+console.log(typeof(a) + "\t" + typeof(b));
+console.log(a + "\t" + b);
+console.log();
+
+let c = 98.67;
+let d = String(c);
+console.log(typeof(c) + "\t" + typeof(d));
+console.log(c + "\t" + d);
+console.log();
+
+let e = true;
+let f = String(e);
+console.log(typeof(e) + "\t" + typeof(f));
+console.log(e + "\t" + f);
+console.log();
+
+let g = false;
+let h = String(g);
+console.log(typeof(g) + "\t" + typeof(h));
+console.log(g + "\t" + h);
+console.log();
+
+let i = 1;
+let j = String(i);
+console.log(typeof(i) + "\t" + typeof(j));
+console.log(i + "\t" + j);
+console.log();
+
+let k = 0;
+let l = String(k);
+console.log(typeof(k) + "\t" + typeof(l));
+console.log(k + "\t" + l);
+console.log();
+
+let m = null;
+let n = String(m);
+console.log(typeof(m) + "\t" + typeof(n));
+console.log(m + "\t" + n);
+console.log();
+
+let o = undefined;
+let p = String(o);
+console.log(typeof(o) + "\t" + typeof(p));
+console.log(o + "\t" + p);
+console.log();
+
+let q = NaN;
+let r = String(q);
+console.log(typeof(q) + "\t" + typeof(r));
+console.log(q + "\t" + r);
+console.log();
+
+let u = -5;
+let v = String(u);
+console.log(typeof(u) + "\t" + typeof(v));
+console.log(u + "\t" + v);
+console.log()
