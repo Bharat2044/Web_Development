962178e17a5b99f68e7c5c7a8d51af48bd658866
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 18:53:14 +0000

    code added

---
+++ 03. JavaScript/04.datatype-typeconversion-in-javascript/02.otherTypeToBooleanConversion.js
@@ -0,0 +1,65 @@
+let a = 'abc';
+let b = Boolean(a);
+console.log(typeof(a) + "\t" + typeof(b));
+console.log(a + "\t" + b);
+console.log();
+
+let c = '123';
+let d = Boolean(c);
+console.log(typeof(c) + "\t" + typeof(d));
+console.log(c + "\t" + d);
+console.log();
+
+let e = '';
+let f = Boolean(e);
+console.log(typeof(e) + "\t" + typeof(f));
+console.log(e + "\t" + f);
+console.log();
+
+let g = ' ';
+let h = Boolean(g);
+console.log(typeof(g) + "\t" + typeof(h));
+console.log(g + "\t" + h);
+console.log();
+
+let i = 1;
+let j = Boolean(i);
+console.log(typeof(i) + "\t" + typeof(j));
+console.log(i + "\t" + j);
+console.log();
+
+let k = 0;
+let l = Boolean(k);
+console.log(typeof(k) + "\t" + typeof(l));
+console.log(k + "\t" + l);
+console.log();
+
+let m = null;
+let n = Boolean(m);
+console.log(typeof(m) + "\t" + typeof(n));
+console.log(m + "\t" + n);
+console.log();
+
+let o = undefined;
+let p = Boolean(o);
+console.log(typeof(o) + "\t" + typeof(p));
+console.log(o + "\t" + p);
+console.log();
+
+let q = NaN;
+let r = Boolean(q);
+console.log(typeof(q) + "\t" + typeof(r));
+console.log(q + "\t" + r);
+console.log();
+
+let s = 56.78;
+let t = Boolean(s);
+console.log(typeof(s) + "\t" + typeof(t));
+console.log(s + "\t" + t);
+console.log();
+
+let u = -5;
+let v = Boolean(u);
+console.log(typeof(u) + "\t" + typeof(v));
+console.log(u + "\t" + v);
+console.log()
