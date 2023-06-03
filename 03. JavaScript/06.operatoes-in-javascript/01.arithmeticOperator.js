3f3f949216de5b2743412478f85157fbd1da20bc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 18:53:27 +0000

    code added

---
+++ 03. JavaScript/06.operatoes-in-javascript/01.arithmeticOperator.js
@@ -0,0 +1,41 @@
+let a = 3;
+let b = 3;
+
+// 1. Addition Operator (+)
+console.log(a + b);
+
+// 2. Subtraction Operator (-)
+console.log(a - b);
+
+// 3. Multiplication Operator (*)
+console.log(a * b);
+
+// 4. Division Operator (/)
+console.log(a / b);
+
+// 5. Remainder Operator (%)
+console.log(a % b);
+
+// 6. Exponentiation Operator (**)
+console.log(a ** b);// 7. Pre Increment
+
+// 7. Pre Increment Operator (++x)
+let c = 5;
+console.log(++c);
+
+// 8. Post Increment Operator (x++)
+let d = 5;
+console.log(d++);
+
+// 9. Pre Decrement Operator (--x)
+let e = 5;
+console.log(--e);
+
+// 10. Post Decrement Operator (x--)
+let f = 5;
+console.log(f--);
+
+
+// Adding A Number and a string
+let x = 10 + " Raj";
+console.log(x);
