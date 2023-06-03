e209bcb1f42e9a34dbe295ed7ed4633302a8bfe1
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 19:17:46 +0000

    code added

---
+++ 03. JavaScript/09. numbers-and-maths-in-javascript/02.maths-in-javascript/script.js
@@ -0,0 +1,40 @@
+console.log(Math);
+console.log();
+
+console.log(Math.abs(4));
+console.log(Math.abs(-4));
+console.log();
+
+console.log(Math.round(4.2));
+console.log(Math.round(4.5));
+console.log(Math.round(4.7));
+console.log();
+
+console.log(Math.ceil(4.2));
+console.log(Math.ceil(4.5));
+console.log(Math.ceil(4.7));
+console.log();
+
+console.log(Math.floor(4.2));
+console.log(Math.floor(4.5));
+console.log(Math.floor(4.7));
+console.log();
+
+console.log(Math.min(4, 5));
+console.log(Math.min(2, 3, 56, 89.88, 2.5));
+console.log();
+
+console.log(Math.max(4, 5));
+console.log(Math.max(2, 3, 56, 89.88, 2.5));
+console.log();
+
+console.log(Math.random());
+console.log(Math.random());
+console.log();
+
+console.log(Math.pow(2, 3));
+console.log(Math.hypot(2, 3));
+console.log(Math.sqrt(50));
+console.log(Math.cbrt(50));
+console.log(Math.clz32(65));
+
