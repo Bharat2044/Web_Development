129c85336a75b08d2c98efee69598c794c448ca8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 18:52:46 +0000

    code added

---
+++ 03. JavaScript/03.datatypes-in-javascript/02.intDatatype.js
@@ -0,0 +1,12 @@
+let x = 123;
+let y = 987.567;
+let z = 0;
+
+console.log(typeof x);
+console.log(x);
+console.log(y);
+console.log(z);
+
+// NUmber datatype maximum and minimum value
+console.log(Number.POSITIVE_INFINITY);
+console.log(Number.NEGATIVE_INFINITY);
