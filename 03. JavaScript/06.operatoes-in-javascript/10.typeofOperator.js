48cd9bb370ee27c677c3b6070aaf83a02b5a1438
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 18:54:01 +0000

    code added

---
+++ 03. JavaScript/06.operatoes-in-javascript/10.typeofOperator.js
@@ -0,0 +1,40 @@
+const myFun = new Function("5 + 2");
+const shape = "round";
+const size = 1;
+const foo = ["Apple", "Mango", "Orange"];
+const today = new Date();
+
+console.log(typeof myFun);
+console.log(typeof shape);
+console.log(typeof size);
+console.log(typeof foo);
+console.log(typeof today);
+console.log(typeof doesntExist);
+console.log();
+
+console.log(typeof 123);
+console.log(typeof 98.66);
+console.log(typeof 0);
+console.log(typeof 'Bharat');
+console.log(typeof "");
+console.log(typeof " ");
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof NaN);
+console.log();
+
+console.log(typeof blur);
+console.log(typeof eval);
+console.log(typeof parseInt);
+console.log(typeof shape.split);
+console.log();
+
+console.log(typeof Date);
+console.log(typeof Function);
+console.log(typeof Math);
+console.log(typeof Option);
+console.log(typeof String);
+console.log(typeof Number);
+console.log(typeof Boolean);
