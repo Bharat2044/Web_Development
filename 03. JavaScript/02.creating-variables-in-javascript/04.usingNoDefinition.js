91aa51875eca7c5e7eb3964600696aef58703564
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-03 18:52:29 +0000

    code added

---
+++ 03. JavaScript/02.creating-variables-in-javascript/04.usingNoDefinition.js
@@ -0,0 +1,16 @@
+a = 1;
+b = 2;
+
+console.log(a);
+console.log(b);
+// console.log(c);      // ReferenceError: c is not defined
+
+{
+    b = 102;
+    c = 3;
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
