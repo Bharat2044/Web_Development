e7a6d13d9000ea6987e0fd095e72ecd787fa2d95
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:45:43 +0000

    code added

---
+++ 03. JavaScript/17.promises-in-javascript/04.promiseEx4/script.js
@@ -0,0 +1,13 @@
+let newPromise = new Promise((resolve, reject) => {
+    
+    let randomNumber = Math.random();
+    console.log(randomNumber);
+
+    if (randomNumber > 0.5) {
+        resolve("The Promise is resolved. The number is greater than 0.5");
+    } else {
+        reject("The Promise is rejected. The number is lesser than 0.5");
+    }
+});
+
+newPromise.then((result) => console.log(result));
