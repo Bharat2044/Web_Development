6ec7c5698543436c0a5ed0227344a0c48b6a0c7b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:47:54 +0000

    code added

---
+++ 03. JavaScript/19.important-methods-in-javascript/04.reduce()-method/script.js
@@ -0,0 +1,8 @@
+// shopping cart
+const cartBill = [20, 30, 50]
+const sumOfCartBill = cartBill.reduce((prev, curr) => prev+curr, 0)
+console.log(sumOfCartBill);
+
+let arr = [1, 5, 3, 44, 6];
+let ans = arr.reduce((acc, curr) => acc+curr, 0);
+console.log(ans);
