35d1aaa09f18f083aed5a648f9960c65af7d0f1a
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:42:10 +0000

    code added

---
+++ 03. JavaScript/13.function-in-javascript/08.arrayAsAnArgument.js
@@ -0,0 +1,9 @@
+//function declaration
+function sum([num1, num2, num3]) {
+    return num1+num2+num3;
+}
+
+//calling function
+let numbers = [10, 40, 50]
+let ans = sum(numbers);
+console.log(ans);
