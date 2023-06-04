4eb8ccf92626eb65f445082acd2c2dd9f5dfbeff
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:41:33 +0000

    code added

---
+++ 03. JavaScript/13.function-in-javascript/03.functionWithParameter.js
@@ -0,0 +1,19 @@
+//function declaration
+
+//function with one parameter
+function display(message) {
+    console.log(message);
+}
+
+//function with two parameter
+function sum(num1, num2) {
+    return num1+num2;
+}
+
+//calling function
+display();
+display("Bharat");
+display("Raj Kumar");
+
+let ans = sum(2, 5);
+console.log(ans);
