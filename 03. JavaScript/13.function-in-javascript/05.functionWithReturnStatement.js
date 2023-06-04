982838ef0a47d2c1d386146e25ea560147d7bba8
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:42:02 +0000

    code added

---
+++ 03. JavaScript/13.function-in-javascript/05.functionWithReturnStatement.js
@@ -0,0 +1,10 @@
+//function declaration
+function printMessage() {
+    return "Hello, Everyone!";
+}
+
+//calling function
+console.log(printMessage());
+
+let ans = printMessage() + " How are you?";
+console.log(ans);
