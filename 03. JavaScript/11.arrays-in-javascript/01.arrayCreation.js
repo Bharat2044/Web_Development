014f3a050612e4ebcb374c356d45eaa9666a9d51
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:40:51 +0000

    code added

---
+++ 03. JavaScript/11.arrays-in-javascript/01.arrayCreation.js
@@ -0,0 +1,16 @@
+//array creation
+let a = [1, 2, 3, 'bharat', false];
+console.log(a);
+
+//array creation
+let b = new Array(8, 88, 'raju', true, [1, 2, 3]);
+console.log(b);
+
+//array creation
+let c = Array("abc", "def", "ghi");
+console.log(c);
+
+//accessing array elements
+let arr = [1, 2, 3, 4, 5];
+for(let i=0; i<arr.length; i++)
+    console.log(arr[i], "  ");
