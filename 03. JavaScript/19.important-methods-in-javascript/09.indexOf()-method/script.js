3cd39b698292242f5f6520ac9d9a945182ad1d46
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:48:41 +0000

    code added

---
+++ 03. JavaScript/19.important-methods-in-javascript/09.indexOf()-method/script.js
@@ -0,0 +1,15 @@
+let text = "Hello world, welcome to the universe.";
+let result1 = text.indexOf("welcome");
+console.log(result1);
+
+let result2 = text.indexOf("Welcome");
+console.log(result2);
+
+let result3 = text.indexOf("e");
+console.log(result3);
+
+let result4 = text.indexOf("e", 5);
+console.log(result4);
+
+let result5 = text.indexOf("a");
+console.log(result5);
