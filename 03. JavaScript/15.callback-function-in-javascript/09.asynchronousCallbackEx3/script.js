015a48ccbb690cf177d2f14cbf9c65e9d5100a4b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-04 17:44:30 +0000

    code added

---
+++ 03. JavaScript/15.callback-function-in-javascript/09.asynchronousCallbackEx3/script.js
@@ -0,0 +1,15 @@
+console.log("Start");
+
+setTimeout(function() {
+    console.log("Timer done 1");
+}, 100);
+
+setTimeout(function() {
+    console.log("Timer done 2");
+}, 0);
+
+console.log("End");
+
+for(let i=0; i<10000000000; i++) {
+
+}
