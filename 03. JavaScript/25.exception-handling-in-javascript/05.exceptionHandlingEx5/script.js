f3ebe8c24d1cff64cfa8a101f8eb24806686495c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:48:05 +0000

    code added

---
+++ 03. JavaScript/25.exception-handling-in-javascript/05.exceptionHandlingEx5/script.js
@@ -0,0 +1,13 @@
+console.log("Hello, How are you?");
+console.log("Congratulations !! You have enrolled to the course.");
+console.log("I completed HTML & CSS. Hurray !!");
+
+try {
+console.log(webDevelopment);
+} catch (error) {
+    // console.log(error);
+    console.log(error.message);
+    console.log(error.name);
+}
+
+console.log("You are now learning Javascript");
