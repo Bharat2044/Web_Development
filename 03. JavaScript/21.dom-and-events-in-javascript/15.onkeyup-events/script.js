86e7fb418f8e9e5fcc7d2ff20b86c704f0fbf05f
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:41:52 +0000

    code added

---
+++ 03. JavaScript/21.dom-and-events-in-javascript/15.onkeyup-events/script.js
@@ -0,0 +1,6 @@
+// The onkeyup event occurs when the user releases a key on the keyboard.
+
+function myFunction() {
+    let x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
