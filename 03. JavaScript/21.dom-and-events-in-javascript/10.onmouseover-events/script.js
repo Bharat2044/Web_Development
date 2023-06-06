c727bd9180a78320c553273266ce0631b3650d5b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:40:52 +0000

    code added

---
+++ 03. JavaScript/21.dom-and-events-in-javascript/10.onmouseover-events/script.js
@@ -0,0 +1,6 @@
+// The onmouseover event occurs when the mouse pointer enters an element.
+
+function myFunction() {
+    document.getElementById("demo").style.fontSize = "25px";
+    document.getElementById("demo").style.backgroundColor = "yellow";
+}
