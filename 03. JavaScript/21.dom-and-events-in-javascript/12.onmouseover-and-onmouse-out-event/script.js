4d4a0cfe23068478c58d2952f6920cb3bb771d18
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:41:18 +0000

    code added

---
+++ 03. JavaScript/21.dom-and-events-in-javascript/12.onmouseover-and-onmouse-out-event/script.js
@@ -0,0 +1,11 @@
+// The onmouseout event occurs when the mouse pointer moves out of an element.
+
+function myFunction1() {
+    document.getElementById("demo").style.fontSize = "25px";
+    document.getElementById("demo").style.backgroundColor = "yellow";
+}
+
+function myFunction2() {
+    document.getElementById("demo").style.fontSize = "16px";
+    document.getElementById("demo").style.backgroundColor = "#c9dba2";
+}
