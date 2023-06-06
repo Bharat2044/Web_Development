1b524666bfb31cdf3b69052f91663346f3331d1c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:45:50 +0000

    code added

---
+++ 03. JavaScript/22.set-in-javascript/01.setCreation/script.js
@@ -0,0 +1,50 @@
+// creating empty set
+let emptySet = new Set();
+// Add Values to the Set
+emptySet.add(1);
+emptySet.add(2);
+emptySet.add(3);
+// printing set size
+console.log(emptySet.size);
+// printing Set
+console.log(emptySet);
+console.log();
+
+
+// Create a Set
+const set1 = new Set(["a", "b", "c"]);
+// printing set size
+console.log(set1.size);
+// printing set
+console.log(set1);
+console.log();
+
+
+// Create a Set
+const set2 = new Set();
+// Add Values to the Set
+set2.add("Bharat");
+set2.add(10);
+set2.add("c");
+console.log(set2.size);
+console.log(set2);
+console.log();
+
+
+// Create Variables
+const a = "a";
+const b = 23.7;
+const c = "c";
+
+// Create a Set
+const set3 = new Set();
+// Add Variables to the Set
+set3.add(a);
+set3.add(b);
+set3.add(c);
+console.log(set3.size);
+console.log(set3);
+
+
+
+
