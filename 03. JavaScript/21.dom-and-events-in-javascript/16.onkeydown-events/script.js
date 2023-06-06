ea080507e3f012fc53c6173717d5c4c193789968
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:42:01 +0000

    code added

---
+++ 03. JavaScript/21.dom-and-events-in-javascript/16.onkeydown-events/script.js
@@ -0,0 +1,5 @@
+// The onkeydown event occurs when the user presses a key on the keyboard.
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "You pressed a key inside the input field";
+}
