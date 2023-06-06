7dedd6c67e7e4ed3603653611a88edb7541efb73
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:47:51 +0000

    code added

---
+++ 03. JavaScript/25.exception-handling-in-javascript/03.exceptionHandlingEx3/script.js
@@ -0,0 +1,14 @@
+// let x = undefined;
+// console.log(x[0]);  // TypeError: Cannot read properties of undefined (reading '0')
+
+
+try {
+    let x = undefined;
+    console.log(x[0]);
+} catch (err) {
+    console.log("Error handling in catch", err);
+} finally {
+    console.log("Finally always gets executed");
+}
+
+console.log("End");
