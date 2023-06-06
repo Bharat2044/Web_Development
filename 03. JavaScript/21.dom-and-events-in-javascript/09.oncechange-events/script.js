97b7302ecf79a45abef232ad16f82870bcbe3059
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-06 09:40:46 +0000

    code added

---
+++ 03. JavaScript/21.dom-and-events-in-javascript/09.oncechange-events/script.js
@@ -0,0 +1,6 @@
+// The onchange event is often used in combination with validation of input fields.
+
+function upperCase() {
+    const x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
