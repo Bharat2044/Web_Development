1e0bb4ef9ff927eac8a45c63a41f21d7a08645dc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-07 19:18:53 +0000

    code added

---
+++ 03. JavaScript/26.object-oriented-programming-in-javascript/07.superKeyword/script.js
@@ -0,0 +1,24 @@
+// parent class
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet() {
+        console.log(`Hello ${this.name}`);
+    }
+}
+
+// inheriting parent class
+class Student extends Person {
+
+    constructor(name) {
+        console.log("Creating student class");
+        // call the super class constructor and pass in the name parameter
+        super(name);
+    }
+
+}
+
+let student1 = new Student('Jack');
+student1.greet();
