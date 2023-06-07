3b2ae459863b08037b9e68c428d308d580ab2996
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-07 19:20:25 +0000

    code added

---
+++ 03. JavaScript/28.clouser-in-javascript/1.clouserEx1/script.js
@@ -0,0 +1,20 @@
+let score = 5;
+
+function one() {
+    let score = 10;
+    console.log(score);
+}
+
+function two() {
+    let score = 15;
+    console.log(score);
+}
+
+function three() {
+    console.log(score);
+}
+
+one();
+two();
+three();
+console.log(score);
