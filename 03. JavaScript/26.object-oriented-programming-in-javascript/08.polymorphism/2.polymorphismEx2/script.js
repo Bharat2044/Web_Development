cee12111cd2eb34a8e311d4e9a3db4668b39a3ce
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-07 19:19:06 +0000

    code added

---
+++ 03. JavaScript/26.object-oriented-programming-in-javascript/08.polymorphism/2.polymorphismEx2/script.js
@@ -0,0 +1,14 @@
+const bicycle = {
+    bell: function() {
+        return "Ring, ring! Watch out, please!";
+    }
+}
+
+const door = {
+    bell: function() {
+        return "Ring, ring! Come here, please!";
+    }
+}
+
+console.log(bicycle.bell());
+console.log(door.bell());
