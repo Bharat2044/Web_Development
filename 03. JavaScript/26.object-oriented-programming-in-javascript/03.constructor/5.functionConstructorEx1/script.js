be18018eb5c4e9a1651e16fdb2e776f2aed8a37c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-06-07 19:15:08 +0000

    code added

---
+++ 03. JavaScript/26.object-oriented-programming-in-javascript/03.constructor/5.functionConstructorEx1/script.js
@@ -0,0 +1,29 @@
+function Product(n, p, r) {
+    this.name = n;
+    this.price = p;
+    this.rating = r;
+
+    // return 10;
+    // return {x: 10};
+    // return this;
+}
+
+const p = new Product("iphone", 120000, 4.9);
+console.log(p);
+
+let x = {
+    p: Product
+}
+
+x.p("airpods", 20000.5, 5);
+console.log(x);
+
+/*
+* 1. this keyword in javascript is different than other languages
+* 2. this keyword refers to the context from where we called
+
+* 3. In the function constructor also
+*   - If you return primitive it is ignored and we return the object referred by this
+*   - If you return a custom object, then the custom object is returned
+*   - If you don't returned anything, then object is referred by this is returned
+*/
